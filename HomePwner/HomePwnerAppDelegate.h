7e0cb4b402360e49156ff4a048d955414c100b54
Ryan Case <rmcase@gmail.com>
2013-09-19 16:37:18 -0600

    Initial Commit

---
+++ HomePwner/HomePwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomePwnerAppDelegate.h
+//  HomePwner
+//
+//  Created by Ryan Case on 9/19/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomePwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomePwner
+//
+//  Created by Ryan Case on 9/19/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomePwnerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomePwnerAppDelegate class]));
+    }
+}
