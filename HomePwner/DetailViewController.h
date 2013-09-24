09235c6d79dde05b6d5d1a54c0f2e82163259451
Ryan Case <rmcase@gmail.com>
2013-09-23 18:05:19 -0600

    Finished implementing zoomed pictures, began implementing data persistence

--- HomePwner/DetailViewController.h
+++ HomePwner/DetailViewController.h
@@ -11,19 +11,25 @@
 @class BNRItem;
 
 @interface DetailViewController : UIViewController
-    <UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+    <UITextFieldDelegate, UINavigationControllerDelegate,
+    UIImagePickerControllerDelegate, UIPopoverControllerDelegate>
 {
     __weak IBOutlet UITextField *serialField;
     __weak IBOutlet UITextField *valueField;
     __weak IBOutlet UITextField *nameField;
     __weak IBOutlet UILabel *dateLabel;
-    __weak IBOutlet UIImageView *imageView;
+    __weak IBOutlet UIButton *imageView;
+    
+    UIPopoverController *imagePickerPopover;
 }
 @property (nonatomic, strong) BNRItem *item;
+@property (nonatomic, copy) void (^dismissBlock)(void);
 
 - (IBAction)changeDate:(id)sender;
 - (IBAction)takePicture:(id)sender;
 - (IBAction)backgroundTapped:(id)sender;
 - (IBAction)removeImage:(id)sender;
+- (IBAction)showZoomedPicture:(id)sender;
+- (id)initForNewItem:(BOOL)isNew;
 
 @end
--- HomePwner/ItemsViewController.m
+++ HomePwner/ItemsViewController.m
@@ -70,7 +70,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    DetailViewController *detailViewController = [[DetailViewController alloc] init];
+    DetailViewController *detailViewController = [[DetailViewController alloc] initForNewItem:NO];
     
     // Add the BNRItems array and find the specified BNRItem
     NSArray *items = [[BNRItemStore sharedStore] allItems];
@@ -88,14 +88,21 @@
     // Create a new BNRItem and add it to the store
     BNRItem *newItem = [[BNRItemStore sharedStore] createItem];
     
-    // Figure out where that item is in the array
-    int lastRow = [[[BNRItemStore sharedStore] allItems] indexOfObject:newItem];
+    DetailViewController *detailViewController =
+    [[DetailViewController alloc] initForNewItem:YES];
     
-    NSIndexPath *ip = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    [detailViewController setItem:newItem];
     
-    // Insert this new row into the table.
-    [[self tableView] insertRowsAtIndexPaths:[NSArray arrayWithObject:ip]
-                            withRowAnimation:UITableViewRowAnimationTop];
+    [detailViewController setDismissBlock:^{
+        [[self tableView] reloadData];}
+     ];
+    
+    UINavigationController *navController = [[UINavigationController alloc]
+                                             initWithRootViewController:detailViewController];
+    [navController setModalPresentationStyle:UIModalPresentationFormSheet];
+    [navController setModalTransitionStyle:UIModalTransitionStyleFlipHorizontal];
+    
+    [self presentViewController:navController animated:YES completion:nil];
 }
 
 // Commits the deletion request
