5813db8832867a5b15e83cf6b6e4b2634259c69b
Ryan Case <rmcase@gmail.com>
2013-09-24 18:26:31 -0600

    Began implementing custom table cells

---
+++ HomePwner/HomePwnerItemCell.h
@@ -0,0 +1,13 @@
+//
+//  HomePwnerItemCell.h
+//  HomePwner
+//
+//  Created by Ryan Case on 9/24/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePwnerItemCell : UITableViewCell
+
+@end
---
+++ HomePwner/HomePwnerItemCell.m
@@ -0,0 +1,29 @@
+//
+//  HomePwnerItemCell.m
+//  HomePwner
+//
+//  Created by Ryan Case on 9/24/13.
+//  Copyright (c) 2013 Ryan Case. All rights reserved.
+//
+
+#import "HomePwnerItemCell.h"
+
+@implementation HomePwnerItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
